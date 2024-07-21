15f0777d9a6013416d98e6959a68ca0979274813
Uriel <urielotesosa@gmail.com>
2024-07-21 17:02:22 -0300

    testeando

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 const publicPath = path.resolve(__dirname, "./public");
 app.use( express.static(publicPath));
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor corriendo en el puerto 3000");
+app.listen(process.env.PORT || 3200, function() {
+    console.log("Servidor corriendo en el puerto 3200");
 });
 
 app.get("/", (req, res) => {
